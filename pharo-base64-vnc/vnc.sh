3244a8fc982afa7af6aa1632a1578510b2d86627
Christer Assmundsson <Gotchas99@users.noreply.github.com>
2017-10-14 20:03:21 +0200

    Update vnc.sh

--- pharo-base64-vnc/vnc.sh
+++ pharo-base64-vnc/vnc.sh
@@ -3,7 +3,7 @@
 # Remove VNC lock (if process already killed)
 rm /tmp/.X1-lock /tmp/.X11-unix/X1
 # Run VNC server with tail in the foreground
-vncserver :1 -geometry 1280x800 -depth 24 -rfbauth /opt/vncpasswd
+vncserver :1 -geometry 1280x800 -depth 24
 # && tail -F /root/.vnc/*.log #to keep the container from terminating because the CMD did.
 set DISPLAY=:1
 
