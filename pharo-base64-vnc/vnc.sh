f015837c815db7e7a409891ff6e99bb5f02ea8d6
Christer Assmundsson <Gotchas99@users.noreply.github.com>
2017-10-14 13:54:47 +0200

    Create vnc.sh

---
+++ pharo-base64-vnc/vnc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Remove VNC lock (if process already killed)
+rm /tmp/.X1-lock /tmp/.X11-unix/X1
+# Run VNC server with tail in the foreground
+vncserver :1 -geometry 1280x800 -depth 24
+# && tail -F /root/.vnc/*.log #to keep the container from terminating because the CMD did.
+set DISPLAY=:1
+
+./pharo-ui -vm-display-X11 -display :1 Pharo.image --no-quit
