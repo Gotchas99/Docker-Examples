ecc2772d30fb84c862106ec8730bf40c1b03f87f
v0c0056 <christer.assmundsson@consultant.volvo.com>
2017-10-21 19:14:36 +0200

    reset file permissions for vnc config

--- pharo-base64-vnc/vnc.sh
+++ pharo-base64-vnc/vnc.sh
@@ -2,6 +2,9 @@
 
 # Remove VNC lock (if process already killed)
 rm /tmp/.X1-lock /tmp/.X11-unix/X1
+# reset file permissions
+chmod 0700 .vnc \
+	&& chmod 0600 .vnc/passwd
 # Run VNC server with tail in the foreground
 vncserver :1 -geometry 1280x800 -depth 24
 # && tail -F /root/.vnc/*.log #to keep the container from terminating because the CMD did.
