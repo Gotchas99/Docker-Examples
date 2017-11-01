1cfd9a40b3d13660c76a7beb6c60b533deb9e36d
v0c0056 <christer.assmundsson@consultant.volvo.com>
2017-11-01 19:16:12 +0100

    sdfghjk

--- pharo-base64-vnc/vnc.sh
+++ pharo-base64-vnc/vnc.sh
@@ -3,8 +3,8 @@
 # Remove VNC lock (if process already killed)
 rm /tmp/.X1-lock /tmp/.X11-unix/X1
 # reset file permissions
-chmod 0700 .vnc \
-	&& chmod 0600 .vnc/passwd
+chmod 0700 .vnc
+chmod 0600 .vnc/passwd
 # Run VNC server with tail in the foreground
 vncserver :1 -geometry 1280x800 -depth 24
 # && tail -F /root/.vnc/*.log #to keep the container from terminating because the CMD did.
