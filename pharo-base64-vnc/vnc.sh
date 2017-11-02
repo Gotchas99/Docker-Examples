9c04e0c043f214081b60c04ba4a6790b1aa10bc5
v0c0056 <christer.assmundsson@consultant.volvo.com>
2017-11-02 20:21:09 +0100

    fixes to vnc.sh in the base64 folder

--- pharo-base64-vnc/vnc.sh
+++ pharo-base64-vnc/vnc.sh
@@ -2,8 +2,10 @@
 
 # Remove VNC lock (if process already killed)
 rm /tmp/.X1-lock /tmp/.X11-unix/X1
-# reset file permissions
+# copy vnc config & set permissions
+mkdir .vnc 
 chmod 0700 .vnc
+cp vncpasswd .vnc/passwd 
 chmod 0600 .vnc/passwd
 # Run VNC server with tail in the foreground
 vncserver :1 -geometry 1280x800 -depth 24
--- pharo-vnc/vnc.sh
+++ pharo-vnc/vnc.sh
@@ -3,7 +3,6 @@
 # Remove VNC lock (if process already killed)
 rm /tmp/.X1-lock /tmp/.X11-unix/X1
 # copy vnc config & set permissions
-
 mkdir .vnc 
 chmod 0700 .vnc 
 cp vncpasswd .vnc/passwd 
@@ -13,4 +12,4 @@ vncserver :1 -geometry 1280x800 -depth 24
 # && tail -F /root/.vnc/*.log #to keep the container from terminating because the CMD did.
 set DISPLAY=:1
 
-./pharo-ui -vm-display-X11 -display :1 Pharo.image --no-quit
+./pharo-ui -vm-display-X11 --display :1 Pharo.image --no-quit
