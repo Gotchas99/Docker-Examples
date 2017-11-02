87255fad006036cfaf134e4a15c63638b72dc786
v0c0056 <christer.assmundsson@consultant.volvo.com>
2017-11-02 17:52:21 +0100

    moved .vnc to script

--- pharo-vnc/vnc.sh
+++ pharo-vnc/vnc.sh
@@ -2,6 +2,12 @@
 
 # Remove VNC lock (if process already killed)
 rm /tmp/.X1-lock /tmp/.X11-unix/X1
+# copy vnc config & set permissions
+
+mkdir .vnc 
+chmod 0700 .vnc 
+cp vncpasswd .vnc/passwd 
+chmod 0600 .vnc/passwd
 # Run VNC server with tail in the foreground
 vncserver :1 -geometry 1280x800 -depth 24
 # && tail -F /root/.vnc/*.log #to keep the container from terminating because the CMD did.
