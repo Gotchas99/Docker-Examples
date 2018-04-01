20ac765e3d479405c5b0284eb9b717610ccfce9d
v0c0056 <christer.assmundsson@consultant.volvo.com>
2018-04-01 10:59:07 +0200

    fixed proxy script

--- pharo-teapot/build.cmd
+++ pharo-teapot/build.cmd
@@ -1,3 +1,11 @@
-::docker build --build-arg http_proxy="http://httppxgot.srv.volvo.com:8080" --build-arg https_proxy="http://httppxgot.srv.volvo.com:8080" -t gotchas/pharo-teapot .
-docker build --force-rm --build-arg http_proxy="http://172.22.64.1:3128" --build-arg https_proxy="http://172.22.64.1:3128" -t gotchas/pharo-teapot --no-cache .
-docker push gotchas/pharo-teapot
+set IMG=gotchas/pharo-teapot
+SET OPTS=
+set PX=http://172.22.64.1:3128
+::set PX=http://pxgot5.srv.volvo.com:8080
+::set PX=http://153.112.38.30:8080
+::set OPTS=%OPTS%  --build-arg http_proxy="%PX%" --build-arg https_proxy="%PX%" 
+set OPTS=%OPTS% --force-rm --no-cache
+docker build %OPTS%  -t %IMG% %1 .
+IF %ERRORLEVEL% NEQ 0 GOTO END
+docker push %IMG%
+:END
