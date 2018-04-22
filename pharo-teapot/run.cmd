8bedebe0e4edfa1351fe5a63e8b69cb31bbbd3b3
v0c0056 <christer.assmundsson@consultant.volvo.com>
2018-04-22 09:57:40 +0200

    cmd and docker updates

--- pharo-teapot/run.cmd
+++ pharo-teapot/run.cmd
@@ -1,2 +1,12 @@
-docker pull gotchas/pharo-teapot
-docker run -d --rm -p 1702:1701 -p 5901:5901 gotchas/pharo-teapot
+set IMG=gotchas/pharo-teapot
+docker pull %IMG%
+SET OPTS=-it --rm 
+SET OPTS=%OPTS% -p 1702:1701 -p 5901:5901
+set PX=http://172.22.64.1:3128
+::set PX=http://pxgot5.srv.volvo.com:8080
+::set PX=http://153.112.38.30:8080
+set OPTS=%OPTS% -e http_proxy="%PX%" -e https_proxy="%PX%" 
+
+docker run %OPTS%  %IMG% %1 
+
+:END
--- pharo61/build.cmd
+++ pharo61/build.cmd
@@ -1,10 +1,9 @@
 set IMG=gotchas/pharo61
-SET OPTS=
+set OPTS=--force-rm --no-cache --squash
 set PX=http://172.22.64.1:3128
 ::set PX=http://pxgot5.srv.volvo.com:8080
 ::set PX=http://153.112.38.30:8080
 set OPTS=%OPTS%  --build-arg http_proxy="%PX%" --build-arg https_proxy="%PX%" 
-set OPTS=%OPTS% --force-rm --no-cache
 docker build %OPTS%  -t %IMG% %1 .
 IF %ERRORLEVEL% NEQ 0 GOTO END
 docker push %IMG%
