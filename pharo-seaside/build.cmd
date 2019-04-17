22e5ea5f945459e8f352b2e78cdbb9ab56a323d6
v0c0056 <christer.assmundsson@consultant.volvo.com>
2019-04-17 10:03:32 +0200

    added most DONT_USE

---
+++ pharo-seaside/build.cmd
@@ -0,0 +1,10 @@
+set IMG=gotchas/pharo-seaside
+set OPTS=--force-rm --no-cache --squash
+::set PX=http://172.22.64.1:3128
+::set PX=http://pxgot5.srv.volvo.com:8080
+set PX=http://v0c0056:Gotchas323@153.112.38.30:8080
+set OPTS=%OPTS%  --build-arg http_proxy="%PX%" --build-arg https_proxy="%PX%" 
+docker build %OPTS%  -t %IMG% %1 .
+IF %ERRORLEVEL% NEQ 0 GOTO END
+docker push %IMG%
+:END
